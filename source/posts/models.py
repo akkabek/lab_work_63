00fba961bbf2141f3aca49edf59cf90f4bd340d5
Bektur_Akkaziev <bektur@MacBook-Pro-Bektur.local>
2026-07-06 17:29:36 +0600

    Добавлена модель постов

--- source/posts/models.py
+++ source/posts/models.py
@@ -1,3 +1,21 @@
+from django.conf import settings
 from django.db import models
 
-# Create your models here.
+
+class Post(models.Model):
+
+    author = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        related_name='posts',
+        on_delete=models.CASCADE,
+        verbose_name='Автор',
+    )
+    image = models.ImageField(upload_to='posts/', verbose_name='Изображение')
+    caption = models.TextField(blank=True, verbose_name='Описание')
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ['-created_at']
+
+    def __str__(self):
+        return f'{self.author}: {self.caption[:30]}'
