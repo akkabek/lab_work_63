a7cdb800e1d251a3b2edb662b1c5e6ff7e926e7e
Bektur_Akkaziev <bektur@MacBook-Pro-Bektur.local>
2026-07-06 17:32:52 +0600

    Добавлено добавление публикации

--- source/posts/views.py
+++ source/posts/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.urls import reverse
+from django.views.generic import CreateView
 
-# Create your views here.
+from posts.forms import PostForm
+
+
+class PostCreateView(LoginRequiredMixin, CreateView):
+
+    form_class = PostForm
+    template_name = 'posts/post_form.html'
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+
+    def get_success_url(self):
+        return reverse('posts:detail', kwargs={'pk': self.object.pk})
