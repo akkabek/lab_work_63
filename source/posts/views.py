c30d6021fdef1fea93bad988274042bf4bb6525b
Bektur_Akkaziev <bektur@MacBook-Pro-Bektur.local>
2026-07-06 17:55:22 +0600

    Добавлена вьюшка лайка публикации

--- source/posts/views.py
+++ source/posts/views.py
@@ -1,8 +1,11 @@
 from django.contrib.auth.mixins import LoginRequiredMixin
+from django.shortcuts import get_object_or_404, redirect
 from django.urls import reverse
+from django.views import View
 from django.views.generic import CreateView, TemplateView
 
 from posts.forms import PostForm
+from posts.models import Like, Post
 
 
 class PostCreateView(LoginRequiredMixin, CreateView):
@@ -17,3 +20,9 @@ class PostCreateView(LoginRequiredMixin, CreateView):
     def get_success_url(self):
         return reverse('accounts:profile', kwargs={'pk': self.object.pk})
 
+class PostLikeView(LoginRequiredMixin, View):
+
+    def post(self, request, pk):
+        post = get_object_or_404(Post, pk=pk)
+        Like.objects.get_or_create(user=request.user, post=post)
+        return redirect('posts:detail', pk=post.pk)
