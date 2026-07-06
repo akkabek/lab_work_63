ab12e3032bee686adfc4027af3367f1e5097f53f
Bektur_Akkaziev <bektur@MacBook-Pro-Bektur.local>
2026-07-06 16:09:41 +0600

    Реализована админка для User

--- source/accounts/admin.py
+++ source/accounts/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 
-# Register your models here.
+from .models import User
+
+
+@admin.register(User)
+class CustomUserAdmin(UserAdmin):
+    fieldsets = UserAdmin.fieldsets + (('Дополнительная информация', {
+        'fields': ('avatar', 'bio', 'phone_number', 'gender'),
+        }),)
+    list_display = ('username', 'email', 'first_name', 'is_staff')
