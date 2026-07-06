814958b41908ea6518d047893d3edd13d449bc6f
Bektur_Akkaziev <bektur@MacBook-Pro-Bektur.local>
2026-07-06 18:11:15 +0600

    Добавлена вьюшка поиска

--- source/accounts/views.py
+++ source/accounts/views.py
@@ -1,8 +1,9 @@
 from django.contrib.auth import login
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.contrib.auth.views import LoginView, LogoutView
+from django.db.models import Q
 from django.urls import reverse_lazy
-from django.views.generic import CreateView, DetailView
+from django.views.generic import CreateView, DetailView, ListView
 
 from .forms import LoginForm, RegistrationForm
 from .models import User
@@ -41,4 +42,24 @@ class ProfileView(LoginRequiredMixin, DetailView):
         profile_user = self.object
         context['posts'] = profile_user.posts.all()
         context['is_own_profile'] = profile_user == self.request.user
+        return context
+
+class UserSearchView(LoginRequiredMixin, ListView):
+    model = User
+    template_name = 'accounts/search_results.html'
+    context_object_name = 'found_users'
+
+    def get_queryset(self):
+        query = self.request.GET.get('q', '').strip()
+        if not query:
+            return User.objects.none()
+        return User.objects.filter(
+            Q(username__icontains=query)
+            | Q(email__icontains=query)
+            | Q(first_name__icontains=query)
+        ).exclude(pk=self.request.user.pk)
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['query'] = self.request.GET.get('q', '').strip()
         return context
