36f35fd5e7234468258d6c6abf9961e309c344d4
Bektur_Akkaziev <bektur@MacBook-Pro-Bektur.local>
2026-07-06 16:42:58 +0600

    Добавлены настройки редиректа после логина и урл логаута а также медиа урл и рут

--- source/instagram/settings.py
+++ source/instagram/settings.py
@@ -126,4 +126,10 @@ STATIC_URL = 'static/'
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
+MEDIA_URL = 'media/'
+MEDIA_ROOT = BASE_DIR / 'media'
+
+LOGIN_URL = 'accounts:login'
+LOGOUT_REDIRECT_URL = 'accounts:login'
+
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
