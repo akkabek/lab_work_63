6beb82cc9f1487408c3a824940028d8d61e90c39
Bektur_Akkaziev <bektur@MacBook-Pro-Bektur.local>
2026-07-06 18:12:22 +0600

    Добавлен урл поиска

--- source/accounts/urls.py
+++ source/accounts/urls.py
@@ -9,4 +9,5 @@ urlpatterns = [
     path('login/', views.UserLoginView.as_view(), name='login'),
     path('logout/', views.UserLogoutView.as_view(), name='logout'),
     path('profile/<str:username>/', views.ProfileView.as_view(), name='profile'),
+    path('search/', views.UserSearchView.as_view(), name='search'),
 ]
