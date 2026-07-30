133468f2da8d6e1ae01b3def615a3d357c7d6845
Bektur_Akkaziev <bektur@MacBook-Pro-Bektur.local>
2026-07-30 15:38:48 +0600

    Исправлены ошибки

--- source/instagram/urls.py
+++ source/instagram/urls.py
@@ -6,7 +6,7 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('accounts.urls')),
-    path('posts/', include('posts.urls')),
+    path('', include('posts.urls')),
 ]
 
 if settings.DEBUG:
