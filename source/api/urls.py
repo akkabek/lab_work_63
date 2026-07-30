07af4090eef4e0483f3be4cd392621c312e9f73f
Bektur_Akkaziev <bektur@MacBook-Pro-Bektur.local>
2026-07-30 18:31:05 +0600

    Добавлен функционал для комментариев

--- source/api/urls.py
+++ source/api/urls.py
@@ -1,10 +1,11 @@
 from rest_framework import routers
 
-from api.views import PostViewSet
+from api.views import PostViewSet, CommentViewSet
 
 app_name = 'api'
 
 router = routers.DefaultRouter()
 router.register(r'posts', PostViewSet,'post')
+router.register(r'comments', CommentViewSet,'comment')
 
 urlpatterns = router.urls
--- source/api/views.py
+++ source/api/views.py
@@ -3,10 +3,10 @@ from rest_framework.decorators import action
 from rest_framework.permissions import IsAuthenticated, IsAuthenticatedOrReadOnly
 from rest_framework.response import Response
 
-from posts.models import Like, Post
+from posts.models import Like, Post, Comment
 
 from .permissions import IsAuthorOrReadOnly
-from .serializers import PostReadSerializer, PostWriteSerializer
+from .serializers import PostReadSerializer, PostWriteSerializer, CommentSerializer
 
 
 class PostViewSet(viewsets.ModelViewSet):
@@ -42,3 +42,18 @@ class PostViewSet(viewsets.ModelViewSet):
             {'liked': False, 'likes_count': post.likes_count},
             status=status.HTTP_200_OK,
         )
+
+class CommentViewSet(viewsets.ModelViewSet):
+
+    serializer_class = CommentSerializer
+    permission_classes = [IsAuthenticatedOrReadOnly, IsAuthorOrReadOnly]
+
+    def get_queryset(self):
+        queryset = Comment.objects.select_related('author', 'post').all()
+        post_id = self.request.query_params.get('post')
+        if post_id is not None:
+            queryset = queryset.filter(post_id=post_id)
+        return queryset
+
+    def perform_create(self, serializer):
+        serializer.save(author=self.request.user)
